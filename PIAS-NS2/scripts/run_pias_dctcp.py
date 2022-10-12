5b8d5145d966c0bd51ebd98f3d92253c57c182af
wikkizhang <wikkizhang@tencent.com>
2022-10-12 12:05:05 +0800

    add cbs cdf

--- PIAS-NS2/scripts/run_pias_dctcp.py
+++ PIAS-NS2/scripts/run_pias_dctcp.py
@@ -2,6 +2,8 @@ import threading
 import os
 import Queue
 
+base_dir_name = "./"
+
 def worker():
 	while True:
 		try:
@@ -19,11 +21,12 @@ link_rate = 10
 mean_link_delay = 0.0000002
 host_delay = 0.000020
 queueSize = 240
-load_arr = [0.9, 0.8, 0.7, 0.6, 0.5]
+load_arr = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
 connections_per_pair = 1
 meanFlowSize = 1138*1460			# mean size
 paretoShape = 1.05
-flow_cdf = 'CDF_dctcp.tcl'			# CDF Trace
+# flow_cdf = 'CDF_dctcp.tcl'			# CDF Trace
+flow_cdf = 'CDF_CBS.tcl'			# CDF Trace
 
 enableMultiPath = 1
 perflowMP = 0
@@ -43,15 +46,15 @@ drop_prio_ = 'true'
 prio_scheme_ = 2
 deque_prio_ = 'true'
 keep_order_ = 'true'
-prio_num_arr = [1, 8]
+prio_num_arr = [1, 2, 8]
 ECN_scheme_ = 2 #Per-port ECN marking
-pias_thresh_0 = [759*1460, 909*1460, 999*1460, 956*1460, 1059*1460]
-pias_thresh_1 = [1132*1460, 1329*1460, 1305*1460, 1381*1460, 1412*1460]
-pias_thresh_2 = [1456*1460, 1648*1460, 1564*1460, 1718*1460, 1643*1460]
-pias_thresh_3 = [1737*1460, 1960*1460, 1763*1460, 2028*1460, 1869*1460]
-pias_thresh_4 = [2010*1460, 2143*1460, 1956*1460, 2297*1460, 2008*1460]
-pias_thresh_5 = [2199*1460, 2337*1460, 2149*1460, 2551*1460, 2115*1460]
-pias_thresh_6 = [2325*1460, 2484*1460, 2309*1460, 2660*1460, 2184*1460]
+pias_thresh_0 = [759*1460, 909*1460, 999*1460, 956*1460, 1059*1460, 1059*1460, 1059*1460, 1059*1460, 1059*1460]
+pias_thresh_1 = [1132*1460, 1329*1460, 1305*1460, 1381*1460, 1412*1460, 1412*1460, 1412*1460, 1412*1460, 1412*1460]
+pias_thresh_2 = [1456*1460, 1648*1460, 1564*1460, 1718*1460, 1643*1460, 1643*1460, 1643*1460, 1643*1460, 1643*1460]
+pias_thresh_3 = [1737*1460, 1960*1460, 1763*1460, 2028*1460, 1869*1460, 1869*1460, 1869*1460, 1869*1460, 1869*1460]
+pias_thresh_4 = [2010*1460, 2143*1460, 1956*1460, 2297*1460, 2008*1460, 2008*1460, 2008*1460, 2008*1460, 2008*1460]
+pias_thresh_5 = [2199*1460, 2337*1460, 2149*1460, 2551*1460, 2115*1460, 2115*1460, 2115*1460, 2115*1460, 2115*1460]
+pias_thresh_6 = [2325*1460, 2484*1460, 2309*1460, 2660*1460, 2184*1460, 2184*1460, 2184*1460, 2184*1460, 2184*1460]
 
 topology_spt = 16
 topology_tors = 9
@@ -78,7 +81,7 @@ for prio_num_ in prio_num_arr:
 			sys.exit(0)
 
 		#Directory name: workload_scheme_load_[load]
-		directory_name = 'websearch_%s_%d' % (scheme,int(load_arr[i]*100))
+		directory_name = '%s/websearch_%s_prio%d_%d' % (base_dir_name,scheme,int(prio_num_),int(load_arr[i]*100))
 		directory_name = directory_name.lower()
 		#Simulation command
 		cmd = ns_path+' '+sim_script+' '\
